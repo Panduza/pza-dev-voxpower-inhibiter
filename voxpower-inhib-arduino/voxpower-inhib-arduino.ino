99a8ce446c862989eaa501b8907f420b4ab3d968
Lucas Giese <UF073LGI@net.elsys-design.com>
2024-03-22 11:55:16 +0100

    faster!

--- voxpower-inhib-arduino/voxpower-inhib-arduino.ino
+++ voxpower-inhib-arduino/voxpower-inhib-arduino.ino
@@ -12,24 +12,24 @@ void setup() {
 
 void loop() {
   if (Serial.available()) {
-    int t = millis();
+    
     char command = Serial.read();
-    int channel = Serial.readString().toInt(); // Convert ASCII digit to integer
+    int channel = Serial.readStringUntil('\n').toInt(); // Convert ASCII digit to integer
 
     if (command == 'I') {
       // Inhibit channel
       digitalWrite(channel, HIGH);
-      int d = millis() - t;
-      Serial.print(d);
+
     } else if (command == 'E') {
       // Enable channel
       digitalWrite(channel, LOW);
-      int d = millis() - t;
-      Serial.print(d);
+
     } else if (command == 'S') {
       // Get channel state
+
       if (digitalRead(channel) == HIGH) {
         Serial.write('H');
+        
       } else {
         Serial.write('L');
       }
--- voxpower-inhib-python.py
+++ voxpower-inhib-python.py
@@ -7,27 +7,18 @@ time.sleep(2)
 
 def inhibit(channel):
     """Sends an "I" command to activate the specified inhibit channel."""
-    command = "I" + str(channel)  # Construct the command string with "I" prefix
+    command = "I" + str(channel) + "\n"  # Construct the command string with "I" prefix
     arduino.write(command.encode())  # Encode and send the command
-    response = arduino.read().decode()
-    if response == 'i':
-        return True
-    else:
-        return False
+    print(command)
 
 def enable(channel):
     """Sends an "E" command to dactivate the specified inhibit channel."""
-    command = "E" + str(channel)  # Construct the command string with "E" prefix
+    command = "E" + str(channel) + "\n"  # Construct the command string with "E" prefix
     arduino.write(command.encode())  # Encode and send the command
-    response = arduino.read().decode()
-    if response == 'e':
-        return True
-    else:
-        return False
 
 def get_pin_state(channel):
     """Sends an "S" command to request the state of the specified channel and reads the response."""
-    command = "S" + str(channel)  # Construct the command string with "S" prefix
+    command = "S" + str(channel) + "\n"  # Construct the command string with "S" prefix
     arduino.write(command.encode())  # Encode and send the command
 
     # Read response (assuming Arduino sends a single character 'H' or 'L')
@@ -36,19 +27,12 @@ def get_pin_state(channel):
         return True
     elif response == 'L':
         return False
-
-t = time.time()
-
-#inhibit(2)
-
-state = inhibit(2)
-if state:
-    print("Channel 2 is HIGH")
-else:
-    print("Channel 2 is LOW")
-
-d = time.time() - t
-print(d)
+    
+print("start")
+inhibit(9)
+time.sleep(3)
+print("stop")
+enable(9)
 
 
 """
