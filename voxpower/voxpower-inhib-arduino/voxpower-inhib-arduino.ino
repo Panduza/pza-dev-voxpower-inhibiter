909e759562a44d079da3eb36d98907e87876d1a0
Lucas Giese <UF073LGI@net.elsys-design.com>
2024-04-30 14:33:57 +0200

    updated the pins used

--- voxpower/voxpower-inhib-arduino/voxpower-inhib-arduino.ino
+++ voxpower/voxpower-inhib-arduino/voxpower-inhib-arduino.ino
@@ -2,19 +2,28 @@
 
 void setup() {
   // Set pin modes for channels (adjust based on your needs)
-  for (int i = 2; i < 10; i++) {
+  for (int i = 2; i < 6; i++) {
     pinMode(i, OUTPUT);
   }
+  for (int i = 14; i < 18; i++) {
+    pinMode(i, OUTPUT);
+  }
+
 
   // Start serial communication (default baud rate is 9600)
   Serial.begin(115200);
 }
 
 void loop() {
+  
+  //digitalWrite(14, HIGH);
   if (Serial.available()) {
     
     char command = Serial.read();
     int channel = Serial.readStringUntil('\n').toInt(); // Convert ASCII digit to integer
+    if (channel > 5) {
+      channel = channel + 8;
+    }
 
     if (command == 'I') {
       // Inhibit channel
