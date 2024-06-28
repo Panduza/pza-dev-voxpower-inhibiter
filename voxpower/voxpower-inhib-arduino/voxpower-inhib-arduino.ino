bb1be87e9c85c4eacb4719cdbced8a0c2165a613
Lucas-D20 <lucas.giese@elsys-design.com>
2024-06-28 10:30:03 +0200

    add comments

--- voxpower/voxpower-inhib-arduino/voxpower-inhib-arduino.ino
+++ voxpower/voxpower-inhib-arduino/voxpower-inhib-arduino.ino
@@ -2,7 +2,7 @@
 
 void setup() {
 
-  // Set pin modes for channels (adjust based on your needs)
+  // Set pin modes for channels
   for (int i = 2; i < 6; i++) {
     pinMode(i, OUTPUT);
   }
@@ -17,6 +17,7 @@ void setup() {
 
 void loop() {
   if (Serial.available()) {
+    // Receive a message with one character for command to set the pin state and one character for the id of the pin
     char command = Serial.read();
     int channel = Serial.readStringUntil('\n').toInt(); // Convert ASCII digit to integer
     channel += 1; // Channels 1 to 4 of the Voxpower corresponds to the Pin 2 to 5 of the arduino
@@ -41,6 +42,7 @@ void loop() {
       } else {
         Serial.write("L");
       }
+      // Flush the buffer to immediately send the response
       Serial.flush();
     }
   }
